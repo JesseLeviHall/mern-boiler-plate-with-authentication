8208acfa18c91c82b0593fa889c47ed08919877e
jesse h <jesselevihall@gmail.com>
2022-09-03 15:43:44 -0600

    fix

--- server/contollers/auth.js
+++ server/contollers/auth.js
@@ -1,6 +1,29 @@
+import User from "../models/user.js";
+
+/*
+basic signup:
 export const signup = (req, res) => {
-  console.log("REQ BODY ON SIGNUP", req.body);
-  res.json({
-    data: "you have hit the signup endpoint",
+  const { name, email, password } = req.body;
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "This email already in use",
+      });
+    }
+  });
+  let newUser = new User({ name, email, password });
+  newUser.save((err, success) => {
+    if (err) {
+      console.log("SIGN UP ERROR", err);
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json({
+      message: "SignUp Success!",
+    });
   });
 };
+*/
+
+export const signup = (req, res) => {};
